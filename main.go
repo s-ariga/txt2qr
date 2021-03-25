ef5bce75e1603fb70362591547ebee1e8a11c317
Seiichi Ariga <seiichi.ariga@gmail.com>
2021-03-25 09:38:10 +0900

    FIrst commit

---
+++ main.go
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2021 Seiichi Ariga <seiichi.ariga@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+type Flags struct {
+	originalText string
+	outputFile   string
+	level        int
+	size         int
+}
+
+func main() {
+
+	f, err := createAndParseFlags()
+	if err != nil {
+		panic("オプションがエラー")
+	}
+
+	fmt.Println(f.originalText)
+	fmt.Println(f.outputFile)
+
+	err = qrcode.WriteFile(f.originalText, qrcode.Medium, 256, f.outputFile)
+	if err != nil {
+		panic("画像生成でエラー")
+	}
+
+}
+
+func createAndParseFlags() (Flags, error) {
+	f := Flags{
+		originalText: "",
+		outputFile:   "",
+	}
+
+	// オプションを設定
+	flag.StringVar(&f.originalText, "i", "", "QRコードに変換する文字列")
+	flag.StringVar(&f.outputFile, "o", "qr_output.png", "QRコード出力ファイル名")
+	flag.IntVar(&f.level, "l", 2, "QRコードのリカバリーレベル（1:低 ~ 4:高）")
+	flag.IntVar(&f.size, "s", 256, "QRコード画像(正方形)のサイズ")
+
+	if f.level > 4 || f.level < 1 {
+		return f, errors.New("リカバリーレベルは 1~4")
+	}
+
+	// オプションを解析
+	flag.Parse()
+
+	return f, nil
+}
---
+++ readme.md
@@ -0,0 +1,34 @@
+<!--
+ Copyright 2021 Seiichi Ariga <seiichi.ariga@gmail.com>
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+# QRコードを生成するコマンド
+
+Go Lang。授業用のサンプルとして作成したコマンド。CLIのプログラム作成のヒント用
+
+文字列からQRコードのPNGを作る
+
+## オプション
+
+| オプション名 | 意味 |　デフォルト |
+ --- | --- | ---
+| -i | 変換する文字列 | |
+| -o | 出力する画像ファイル名(PNG) | qr_output.png |
+| -s | 画像サイズ | 256 |
+| -l | レベル | 2 (Medium) |
+
+## 依存ライブラリ
+
+[github.com/skip2/go-qrcode](https://github.com/skip2/go-qrcode)
